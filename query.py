935d4ced15752819cefa1e810ebe90ee78a0472b
Elliott Beach <elliott2.71828@gmail.com>
2016-11-12 17:23:12 -0600

    testing stuff

--- query.py
+++ query.py
@@ -5,3 +5,10 @@ api = twitter.Api(consumer_key=consumer_key,
                   consumer_secret=consumer_secret,
                   access_token_key=access_token_key,
                   access_token_secret=access_token_secret)
+
+# results = api.GetSearch(
+#     raw_query="q=webdev&src=typd")
+
+results = api.PostUpdate('test')
+
+print results
