391273b4143761482bea414b1259fec3759f7bfa
Elliott Beach <elliott2.71828@gmail.com>
2016-11-12 19:54:41 -0600

    add first real method

--- query.py
+++ query.py
@@ -1,23 +1,11 @@
+from pprint import pprint
 import twitter
 from credentials import consumer_key, consumer_secret, access_key, access_secret
 
 api= twitter.Twitter(
 		        auth = twitter.OAuth(access_key, access_secret, consumer_key, consumer_secret))
 
-#-----------------------------------------------------------------------
-# perform a basic search 
-# Twitter API docs:
-# https://dev.twitter.com/docs/api/1/get/search
-#-----------------------------------------------------------------------
-query = api.search.tweets(q = "webdev")
-
-#-----------------------------------------------------------------------
-# How long did this query take?
-#-----------------------------------------------------------------------
-print "Search complete (%.3f seconds)" % (query["search_metadata"]["completed_in"])
-
-#-----------------------------------------------------------------------
-# Loop through each of the results, and print its content.
-#-----------------------------------------------------------------------
-for result in query["statuses"]:
-	print "(%s) @%s %s" % (result["created_at"], result["user"]["screen_name"], result["text"])
+def get_users(topic_str):
+	results = api.users.search(q = topic_str)
+	IDs = [ user["id"] for user in results ]
+	return IDs
