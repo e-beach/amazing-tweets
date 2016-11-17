bbe34cfc1ed2040be98cbd94f0253ee8d7b69106
Elliott Beach <elliott2.71828@gmail.com>
2016-11-16 22:16:06 -0600

    add credentials.py

---
+++ credentials.py
@@ -0,0 +1,5 @@
+import os
+consumer_key = os.environ['CONSUMER_KEY']
+consumer_secret = os.environ['CONSUMER_SECRET']
+access_key = os.environ['ACCESS_KEY']
+access_secret = os.environ['ACCESS_SECRET']
